58c19b4dede537b2582825b1d02f3573c03a0574
pingkai <pingkai010@gmail.com>
2020-01-10 14:47:18 +0800

    feat: support Cheater render
    Signed-off-by: pingkai <pingkai010@gmail.com>

---
+++ framework/render/audio/CheaterAudioRender.h
@@ -0,0 +1,53 @@
+//
+// Created by moqi on 2020/1/10.
+//
+
+#ifndef CICADAMEDIA_CHEATERAUDIORENDER_H
+#define CICADAMEDIA_CHEATERAUDIORENDER_H
+
+#include "filterAudioRender.h"
+#include "utils/af_clock.h"
+
+namespace Cicada {
+    class CheaterAudioRender : public filterAudioRender {
+    public:
+        CheaterAudioRender()
+        {
+
+        }
+
+        ~CheaterAudioRender() override
+        {
+
+        }
+
+    private:
+        int init_device() override;
+
+        int pause_device() override;
+
+        int start_device() override;
+
+        void flush_device() override;
+
+        void device_setVolume(float gain) override
+        {
+
+        }
+
+        int64_t device_get_position() override;
+
+        int device_write(unique_ptr<IAFFrame> &frame) override;
+
+        uint64_t device_get_que_duration() override;
+
+    private:
+        af_clock mClock{};
+        int64_t mPCMDuration{0};
+
+
+    };
+}
+
+
+#endif //CICADAMEDIA_CHEATERAUDIORENDER_H
