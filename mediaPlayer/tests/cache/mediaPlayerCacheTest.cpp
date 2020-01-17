4459ff0f692952e85e3d716e54d2c57ca9ed7ca8
pingkai <pingkai010@gmail.com>
2020-01-17 15:00:42 +0800

    test(mediaPlayer): add cached source test
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- mediaPlayer/tests/cache/mediaPlayerCacheTest.cpp
+++ mediaPlayer/tests/cache/mediaPlayerCacheTest.cpp
@@ -8,8 +8,12 @@
 #include <utils/timer.h>
 #include "tests/player_command.h"
 #include <vector>
+#include <data_source/cachedSource.h>
+#include <data_source/SourceReader.h>
+
 
 using namespace std;
+using namespace Cicada;
 
 int main(int argc, char **argv)
 {
@@ -30,10 +34,49 @@ static int OnCreate(Cicada::MediaPlayer *player, void *arg)
     return 0;
 }
 
-TEST(cache, cache)
+TEST(cache, file)
 {
     test_simple("http://player.alicdn.com/video/aliyunmedia.mp4", OnCreate, simple_loop,
                 nullptr, nullptr);
+
+}
+
+SourceReader *g_reader = nullptr;
+
+int readCallBack(void *arg, uint8_t *buffer, int size)
+{
+
+    AF_TRACE;
+    auto *reader = static_cast<SourceReader *>(arg);
+    return reader->read(buffer, size);
+}
+
+int64_t seekCallBack(void *arg, int64_t offset, int whence)
+{
+    AF_TRACE;
+    auto *reader = static_cast<SourceReader *>(arg);
+    return reader->seek(offset, whence);
+
+}
+
+static int cachedSourceOnCreate(Cicada::MediaPlayer *player, void *arg)
+{
+    shared_ptr<cachedSource> source = make_shared<cachedSource>("http://player.alicdn.com/video/aliyunmedia.mp4", 0);
+
+    g_reader = new SourceReader(source);
+    player->setBitStreamCb(readCallBack, seekCallBack, g_reader);
+    //  log_set_level(AF_LOG_LEVEL_DEBUG, 1);
+    return 0;
+
+}
+
+TEST(cache, cachedSource)
+{
+
+    test_simple("", cachedSourceOnCreate, simple_loop, nullptr, nullptr);
+
+    delete g_reader;
+    g_reader = nullptr;
 
 }
 
