60b6b62b7f52082c9fc6246f99bab114a09211ae
pingkai <pingkai010@gmail.com>
2020-02-05 19:24:08 +0800

    chore(framework): fix android build
    add strnstr on android
    
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- framework/utils/af_string.h
+++ framework/utils/af_string.h
@@ -10,6 +10,16 @@
 #include <sstream>
 #include "CicadaType.h"
 
+#ifndef __APPLE__
+#ifdef __cplusplus
+extern "C" {
+#endif
+char *strnstr(const char *s, const char *find, size_t slen);
+#ifdef __cplusplus
+}
+#endif
+#endif
+
 class CICADA_CPLUS_EXTERN AfString {
 public:
     static inline int isSpace(int c)
@@ -17,24 +27,25 @@ public:
         return c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v';
     }
 
-    static void trimString(std::string & str)
+    static void trimString(std::string &str)
     {
         std::size_t n = str.find_last_not_of(" \r\n\t");
-        if (n != std::string::npos){
+        if (n != std::string::npos) {
             str.erase(n + 1, str.size() - n);
         }
         n = str.find_first_not_of(" \r\n\t");
-        if (n != std::string::npos){
+        if (n != std::string::npos) {
             str.erase(0, n);
         }
 
         return;
     }
 
-    static std::vector<std::string> s_split(const std::string& in, const std::string& delim);
+    static std::vector<std::string> s_split(const std::string &in, const std::string &delim);
 
     template<typename T>
-    static std::string to_string(T value) {
+    static std::string to_string(T value)
+    {
 #ifdef ANDROID
         std::ostringstream os;
         os << value;
@@ -45,15 +56,18 @@ public:
     }
 
     static void replaceAll(std::string &data, std::string toSearch, std::string replaceStr);
+
     static std::string make_absolute_url(const std::string &path1, const std::string &path2);
 
     static bool isLocalURL(const std::string &url);
 
-    static inline bool startWith(const std::string &str, const std::string &head) {
+    static inline bool startWith(const std::string &str, const std::string &head)
+    {
         return str.compare(0, head.size(), head) == 0;
     }
 
-    static inline bool endWith(const std::string &str, const std::string &tail) {
+    static inline bool endWith(const std::string &str, const std::string &tail)
+    {
         return str.compare(str.size() - tail.size(), tail.size(), tail) == 0;
     }
 };
