d19cc011c8b2c9edf4afa1400a3b314e312cb89f
pingkai <pingkai010@gmail.com>
2020-02-06 16:12:09 +0800

    refactor(demuxer): add set meta api
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- framework/utils/AFMediaType.h
+++ framework/utils/AFMediaType.h
@@ -44,8 +44,8 @@ enum AFCodecID {
     AF_CODEC_ID_VP9,
 //    AF_CODEC_ID_MJPEG,
 //    AF_CODEC_ID_H263,
-    AF_CODEC_ID_HEVC,
     AF_CODEC_ID_AV1,
+    AF_CODEC_ID_HEVC,
 
     AF_CODEC_ID_AAC,
     AF_CODEC_ID_AC3,
@@ -96,6 +96,41 @@ enum AFSampleFormat {
     AF_SAMPLE_FMT_DBLP,        ///< double, planar
 
     AF_SAMPLE_FMT_NB           ///< Number of sample formats. DO NOT USE if linking dynamically
+};
+enum AFPixelFormat {
+    AF_PIX_FMT_NONE = -1,
+    AF_PIX_FMT_YUV420P,   ///< planar YUV 4:2:0, 12bpp, (1 Cr & Cb sample per 2x2 Y samples)
+    AF_PIX_FMT_YUYV422,   ///< packed YUV 4:2:2, 16bpp, Y0 Cb Y1 Cr
+    AF_PIX_FMT_RGB24,     ///< packed RGB 8:8:8, 24bpp, RGBRGB...
+    AF_PIX_FMT_BGR24,     ///< packed RGB 8:8:8, 24bpp, BGRBGR...
+    AF_PIX_FMT_YUV422P,   ///< planar YUV 4:2:2, 16bpp, (1 Cr & Cb sample per 2x1 Y samples)
+    AF_PIX_FMT_YUV444P,   ///< planar YUV 4:4:4, 24bpp, (1 Cr & Cb sample per 1x1 Y samples)
+    AF_PIX_FMT_YUV410P,   ///< planar YUV 4:1:0,  9bpp, (1 Cr & Cb sample per 4x4 Y samples)
+    AF_PIX_FMT_YUV411P,   ///< planar YUV 4:1:1, 12bpp, (1 Cr & Cb sample per 4x1 Y samples)
+    AF_PIX_FMT_GRAY8,     ///<        Y        ,  8bpp
+    AF_PIX_FMT_MONOWHITE, ///<        Y        ,  1bpp, 0 is white, 1 is black, in each byte pixels are ordered from the msb to the lsb
+    AF_PIX_FMT_MONOBLACK, ///<        Y        ,  1bpp, 0 is black, 1 is white, in each byte pixels are ordered from the msb to the lsb
+    AF_PIX_FMT_PAL8,      ///< 8 bits with AV_PIX_FMT_RGB32 palette
+    AF_PIX_FMT_YUVJ420P,  ///< planar YUV 4:2:0, 12bpp, full scale (JPEG), deprecated in favor of AV_PIX_FMT_YUV420P and setting color_range
+    AF_PIX_FMT_YUVJ422P,  ///< planar YUV 4:2:2, 16bpp, full scale (JPEG), deprecated in favor of AV_PIX_FMT_YUV422P and setting color_range
+    AF_PIX_FMT_YUVJ444P,  ///< planar YUV 4:4:4, 24bpp, full scale (JPEG), deprecated in favor of AV_PIX_FMT_YUV444P and setting color_range
+    AF_PIX_FMT_UYVY422,   ///< packed YUV 4:2:2, 16bpp, Cb Y0 Cr Y1
+    AF_PIX_FMT_UYYVYY411, ///< packed YUV 4:1:1, 12bpp, Cb Y0 Y1 Cr Y2 Y3
+    AF_PIX_FMT_BGR8,      ///< packed RGB 3:3:2,  8bpp, (msb)2B 3G 3R(lsb)
+    AF_PIX_FMT_BGR4,      ///< packed RGB 1:2:1 bitstream,  4bpp, (msb)1B 2G 1R(lsb), a byte contains two pixels, the first pixel in the byte is the one composed by the 4 msb bits
+    AF_PIX_FMT_BGR4_BYTE, ///< packed RGB 1:2:1,  8bpp, (msb)1B 2G 1R(lsb)
+    AF_PIX_FMT_RGB8,      ///< packed RGB 3:3:2,  8bpp, (msb)2R 3G 3B(lsb)
+    AF_PIX_FMT_RGB4,      ///< packed RGB 1:2:1 bitstream,  4bpp, (msb)1R 2G 1B(lsb), a byte contains two pixels, the first pixel in the byte is the one composed by the 4 msb bits
+    AF_PIX_FMT_RGB4_BYTE, ///< packed RGB 1:2:1,  8bpp, (msb)1R 2G 1B(lsb)
+    AF_PIX_FMT_NV12,      ///< planar YUV 4:2:0, 12bpp, 1 plane for Y and 1 plane for the UV components, which are interleaved (first byte U and the following byte V)
+    AF_PIX_FMT_NV21,      ///< as above, but U and V bytes are swapped
+
+    AF_PIX_FMT_D3D11 = 900,
+    AF_PIX_FMT_DXVA2_VLD,
+
+    AF_PIX_FMT_APPLE_PIXEL_BUFFER = 1000,
+    AF_PIX_FMT_CICADA_AF,//framework VideoFrame
+    AF_PIX_FMT_CICADA_MEDIA_CODEC,//Android mediacodec buffer index
 };
 
 typedef enum InterlacedType_t {
@@ -181,6 +216,7 @@ typedef struct {
     int displayWidth;
     int displayHeight;
     double avg_fps;
+    enum AFPixelFormat pixel_fmt;
 
     int pid;
     int no_program;
@@ -197,20 +233,6 @@ typedef struct {
     Source_meta *meta;
 } Stream_meta;
 
-
-enum pix_fmt {
-    AF_PIX_FMT_NONE = -1,
-    AF_PIX_FMT_YUV420P,   ///< planar YUV 4:2:0, 12bpp, (1 Cr & Cb sample per 2x2 Y samples)
-    AF_PIX_FMT_YUVJ420P = 12,  ///< planar YUV 4:2:0, 12bpp, full scale (JPEG), deprecated in favor of PIX_FMT_YUV420P and setting color_range
-    AF_PIX_FMT_YUVJ422P,
-
-    AF_PIX_FMT_D3D11,
-    AF_PIX_FMT_DXVA2_VLD,
-
-    AF_PIX_FMT_APPLE_PIXEL_BUFFER = 1000,
-    AF_PIX_FMT_CICADA_AF,//framework VideoFrame
-    AF_PIX_FMT_CICADA_MEDIA_CODEC,//Android mediacodec buffer index
-};
 
 enum color_space {
     COLOR_SPACE_UNSPECIFIED = 0,
@@ -243,7 +265,7 @@ enum dec_flag {
     dec_flag_direct,
     dec_flag_adaptive,
     // adjust setting to output frames as soon as possiable.
-            dec_flag_output_frame_asap,
+    dec_flag_output_frame_asap,
 };
 #define DECFLAG_DUMMY  1u << dec_flag_dummy
 #define DECFLAG_HW     (1u << dec_flag_hw)
