fbe28c1fbb7c91eb6c17f27a533082188c1762c1
pingkai <pingkai010@gmail.com>
2020-03-11 16:44:06 +0800

    style(ffmpegmuxer): not use namespace in header file

--- framework/muxer/ffmpegMuxer/FfmpegMuxer.h
+++ framework/muxer/ffmpegMuxer/FfmpegMuxer.h
@@ -17,29 +17,23 @@ extern "C" {
 #include <libavformat/avformat.h>
 };
 
-using namespace std;
-
 #include <muxer/IMuxerPrototype.h>
 
 
 class CICADA_CPLUS_EXTERN FfmpegMuxer : public IMuxer, private IMuxerPrototype {
 public:
-    FfmpegMuxer(string destFilePath, string destFormat);
+    FfmpegMuxer(std::string destFilePath, std::string destFormat);
 
-    virtual ~FfmpegMuxer() override;
-
-public:
+    ~FfmpegMuxer() override;
 
     void setCopyPts(bool copyPts) override;
 
+    void setStreamMetas(const std::vector<Stream_meta *> &streamMetas) override;
 
-    void setStreamMetas(const vector<Stream_meta*> &streamMetas) override;
-
-
-    void clearStreamMetas() override ;
+    void clearStreamMetas() override;
 
     //must be set before open(). These will be write to header.
-    void addSourceMetas(map<string, string> sourceMetas) override;
+    void addSourceMetas(std::map<std::string, std::string> sourceMetas) override;
 
     void setWritePacketCallback(writePacketCallback callback, void *opaque) override;
 
@@ -47,43 +41,46 @@ public:
 
     void setWriteDataTypeCallback(writeDataTypeCallback callback, void *opaque) override;
 
-    void setOpenFunc(function<void()> func) override;
+    void setOpenFunc(std::function<void()> func) override;
 
-    void setCloseFunc(function<void()> func) override;
+    void setCloseFunc(std::function<void()> func) override;
 
     int open() override;
 
-    int muxPacket(unique_ptr<IAFPacket> packet) override;
+    int muxPacket(std::unique_ptr<IAFPacket> packet) override;
 
     int close() override;
 
 protected:
-    explicit FfmpegMuxer(int dummy) {
+    explicit FfmpegMuxer(int dummy)
+    {
         addPrototype(this);
     }
 
 private:
     IMuxer *
-    clone(const string &destPath, const string &destFormat, const string &description) override {
+    clone(const std::string &destPath, const std::string &destFormat, const std::string &description) override
+    {
         return new FfmpegMuxer(destPath, destFormat);
     }
 
-    int probeScore(const string &destPath, const string &destFormat,
-                   const string &description) override {
-        if(is_supported(destPath , destFormat,description)){
+    int probeScore(const std::string &destPath, const std::string &destFormat,
+                   const std::string &description) override
+    {
+        if (is_supported(destPath, destFormat, description)) {
             return Cicada::SUPPORT_DEFAULT;
         }
         return 0;
     }
 
-    bool is_supported(const string &destPath, const string &destFormat,
-                      const string &description)  {
+    static bool is_supported(const std::string &destPath, const std::string &destFormat,
+                             const std::string &description)
+    {
         return true;
     }
 
     static FfmpegMuxer se;
 
-private:
     class StreamInfo {
     public:
         int targetIndex = -1;
@@ -112,7 +109,7 @@ private:
     static int io_write_data_type(void *opaque, uint8_t *buf, int size,
                                   enum AVIODataMarkerType type, int64_t time);
 
-    int writeFrame(unique_ptr<IAFPacket> packetPtr);
+    int writeFrame(std::unique_ptr<IAFPacket> packetPtr);
 
     void check_codec_tag(const AVStream *stream);
 
@@ -122,9 +119,9 @@ protected:
 
 private:
 
-    map<string, string> mSourceMetaMap;
-    vector<Stream_meta*> mStreamMetas;
-    map<int, StreamInfo> mStreamInfoMap;
+    std::map<std::string, std::string> mSourceMetaMap;
+    std::vector<Stream_meta *> mStreamMetas;
+    std::map<int, StreamInfo> mStreamInfoMap;
 
     uint8_t *mIobuf = nullptr;
     seekCallback mSeekCallback = nullptr;
@@ -134,13 +131,13 @@ private:
     writeDataTypeCallback mWriteDataTypeCallback = nullptr;
     void *mWriteDataTypeOpaque = nullptr;
 
-    function<void()> mOpenFunc = nullptr;
-    function<void()> mCloseFunc = nullptr;
+    std::function<void()> mOpenFunc = nullptr;
+    std::function<void()> mCloseFunc = nullptr;
     int64_t mFirstPts = INT64_MIN;
 
 protected:
-    string mDestFilePath;
-    string mDestFormat;
+    std::string mDestFilePath;
+    std::string mDestFormat;
 
     bool bCopyPts = false;
 
