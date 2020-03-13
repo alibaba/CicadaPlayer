04feda30c96f701847d37a938bc40e116ee067a7
lifujun <814084764@qq.com>
2020-03-13 18:51:39 +0800

    refactor(md5): move api into class

--- framework/utils/Md5Utils.h
+++ framework/utils/Md5Utils.h
@@ -12,16 +12,16 @@ namespace Cicada {
     class Md5Utils {
     public:
         static std::string getMd5(const std::string &source);
-    };
-//    class SHA1Utils{
-//    public:
-//        static std::string getSha256(const std::string &source);
-//    };
 
+        static void MD5(unsigned char *src, int len, unsigned char *dst);
+    };
+    class SHA1Utils{
+    public:
+//        static std::string getSha256(const std::string &source);
+
+        static unsigned char *SHA256(const unsigned char *d, size_t n, unsigned char *md);
+    };
 }
 
-unsigned char *SHA256(const unsigned char *d, size_t n, unsigned char *md);
-
-void MD5(unsigned char *src, int len, unsigned char *dst);
 
 #endif //SOURCE_MD5UTILS_H
