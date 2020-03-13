6a2034686cd3e809a202129331f606461af0e23f
lifujun <814084764@qq.com>
2020-03-13 18:51:39 +0800

    refactor(md5): move api into class

--- framework/utils/Md5Utils.cpp
+++ framework/utils/Md5Utils.cpp
@@ -50,39 +50,40 @@ namespace Cicada {
 //        return std::string(out);
 //
 //    }
-}
 
-unsigned char *SHA256(const unsigned char *d, size_t n, unsigned char *md)
-{
-    struct AVSHA *sha = av_sha_alloc();
-    static unsigned char m[32];
 
-    if (md == nullptr) {
-        md = m;
+    unsigned char *SHA1Utils::SHA256(const unsigned char *d, size_t n, unsigned char *md)
+    {
+        struct AVSHA *sha = av_sha_alloc();
+        static unsigned char m[32];
+
+        if (md == nullptr) {
+            md = m;
+        }
+
+        av_sha_init(sha, 256);
+        av_sha_update(sha, d, n);
+        av_sha_final(sha, md);
+        av_free(sha);
+        return md;
     }
 
-    av_sha_init(sha, 256);
-    av_sha_update(sha, d, n);
-    av_sha_final(sha, md);
-    av_free(sha);
-    return md;
-}
+    void  Md5Utils::MD5(unsigned char *src, int len, unsigned char *dst)
+    {
+        static unsigned char outTmp[16] = {0};
 
-void MD5(unsigned char *src, int len, unsigned char *dst)
-{
-    static unsigned char outTmp[16] = {0};
+        if (dst == nullptr) {
+            dst = outTmp;
+        }
 
-    if (dst == nullptr) {
-        dst = outTmp;
+        struct AVMD5 *ctx = av_md5_alloc();
+
+        av_md5_init(ctx);
+
+        av_md5_update(ctx, src, len);
+
+        av_md5_final(ctx, dst);
+
+        av_free(ctx);
     }
-
-    struct AVMD5 *ctx = av_md5_alloc();
-
-    av_md5_init(ctx);
-
-    av_md5_update(ctx, src, len);
-
-    av_md5_final(ctx, dst);
-
-    av_free(ctx);
 }
