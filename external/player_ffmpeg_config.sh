f561ffb08aaf6dc3d2771092b4ed0570d1978d3b
pingkai <pingkai010@gmail.com>
2020-03-17 11:13:39 +0800

    chore(external): disable dav1 prebuilt
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- external/player_ffmpeg_config.sh
+++ external/player_ffmpeg_config.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+function dav1d_decoder_prebuilt() {
+    if [[ -n "${DAV1D_EXTERNAL_DIR}" ]];then
+        if [[ -d "${DAV1D_EXTERNAL_DIR}/$TARGET_PLATFORM/$TARGET_ARCH" ]];then
+            DAV1D_INSTALL_DIR="${DAV1D_EXTERNAL_DIR}/$TARGET_PLATFORM/$TARGET_ARCH"
+        else
+            DAV1D_INSTALL_DIR=
+        fi
+    fi
+
+    echo "DAV1D_INSTALL_DIR is $DAV1D_INSTALL_DIR"
+}
+
 ffmpeg_config_add_decoders aac aac_latm h264 hevc mpeg4 mp3 mp3adu mp3float mp3on4float mp3adufloat mp3on4 pcm_s16le ac3_at
 ffmpeg_config_add_demuxers flv aac live_flv webvtt mov mp3 mpegts matroska h264
 ffmpeg_config_add_muxers mp4 adts mpegts
@@ -16,13 +28,3 @@ ffmpeg_config_add_protocols http https
 if [[ "$TARGET_PLATFORM" != "Android" ]];then
     ffmpeg_config_add_protocols udp
 fi
-
-if [[ -n "${DAV1D_EXTERNAL_DIR}" ]];then
-    if [[ -d "${DAV1D_EXTERNAL_DIR}/$TARGET_PLATFORM/$TARGET_ARCH" ]];then
-        DAV1D_INSTALL_DIR="${DAV1D_EXTERNAL_DIR}/$TARGET_PLATFORM/$TARGET_ARCH"
-    else
-        DAV1D_INSTALL_DIR=
-    fi
-fi
-
-echo "DAV1D_INSTALL_DIR is $DAV1D_INSTALL_DIR"
