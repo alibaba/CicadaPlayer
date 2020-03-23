4f77f1ff25a99dccc6846706618d0016bd3c0b48
pingkai <pingkai010@gmail.com>
2020-03-23 16:20:15 +0800

    build(framework): add option to enable codecs
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- framework/codec/decoderFactory.cpp
+++ framework/codec/decoderFactory.cpp
@@ -36,19 +36,25 @@ unique_ptr<IDecoder> decoderFactory::createBuildIn(const AFCodecID &codec, uint6
 {
     if (flags & DECFLAG_HW) {
 #ifdef ANDROID
+#ifdef ENABLE_MEDIA_CODEC_DECODER
         return std::unique_ptr<IDecoder>(new mediaCodecDecoder());
 #endif
+#endif
 #ifdef __APPLE__
+#ifdef ENABLE_VTB_DECODER
 
         if (AFVTBDecoder::is_supported(codec)) {
             return unique_ptr<IDecoder>(new AFVTBDecoder());
         }
 
 #endif
+#endif
     }
 
     if (flags & DECFLAG_SW) {
+#ifdef ENABLE_AVCODEC_DECODER
         return unique_ptr<IDecoder>(new avcodecDecoder());
+#endif
     }
 
     return nullptr;
