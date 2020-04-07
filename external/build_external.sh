a76b5b8358c818e7697222040420f1fd79db422a
pingkai <pingkai010@gmail.com>
2020-04-07 10:49:59 +0800

    feat(external): add flvdec aliyun extend audio codec id in ffmpeg
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- external/build_external.sh
+++ external/build_external.sh
@@ -24,6 +24,7 @@ function patch_ffmpeg() {
     git_am_patch ../../contribute/ffmpeg/0005-chore-disable-libdav1d-check.patch
     git_am_patch ../../contribute/ffmpeg/0006-flv-add-extensions-for-H.265-HEVC.patch
     git_am_patch ../../contribute/ffmpeg/0007-build-libavformat-compile-avc.c-and-hevc.c-default.patch
+    git_am_patch ../../contribute/ffmpeg/0008-avformat-flvdec-add-aliyun-extend-auio-codec-id.patch
 }
 
 function git_apply_patch() {
