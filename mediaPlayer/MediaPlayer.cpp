5f6aa507d6f1ba04b8cbaf1d5d67a714945ca6e7
skufly <skufly@163.com>
2020-04-14 20:10:03 +0800

    refactor(player): shouldn't reset highBufferDuration to 0

--- mediaPlayer/MediaPlayer.cpp
+++ mediaPlayer/MediaPlayer.cpp
@@ -448,7 +448,8 @@ namespace Cicada {
         }
 
         //must
-        if (playerConfig.maxDelayTime < playerConfig.highBufferDuration) {
+        if ((0 < playerConfig.maxDelayTime)
+                && (playerConfig.maxDelayTime < playerConfig.highBufferDuration)) {
             playerConfig.highBufferDuration = playerConfig.maxDelayTime;
         }
 
