d3d6bba115d78f84262d4745a73d00c756ba5d66
skufly <skufly@163.com>
2020-04-14 15:32:31 +0800

    refactor(demuxer): avoid ANR if enter wait after notify

--- framework/demuxer/avFormatDemuxer.cpp
+++ framework/demuxer/avFormatDemuxer.cpp
@@ -574,7 +574,9 @@ namespace Cicada {
             std::unique_lock<std::mutex> waitLock(mQueLock);
 
             if (bEOS) {
-                mQueCond.wait(waitLock);
+                mQueCond.wait(waitLock, [this]() {
+                    return bPaused || mInterrupted;
+                });
             }
         }
 
@@ -589,7 +591,9 @@ namespace Cicada {
             std::unique_lock<std::mutex> waitLock(mQueLock);
 
             if (mPacketQueue.size() > MAX_QUEUE_SIZE) {
-                mQueCond.wait(waitLock);
+                mQueCond.wait(waitLock, [this]() {
+                    return mPacketQueue.size() <= MAX_QUEUE_SIZE || bPaused || mInterrupted;
+                });
             }
 
             mPacketQueue.push_back(std::move(pkt));
