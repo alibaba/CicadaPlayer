ad2ff7050634195eb6cf8540a6604172642b38b7
skufly <skufly@163.com>
2020-04-30 18:19:14 +0800

    feat(demo): add sync player

---
+++ platform/Apple/demo/macOS/CicadaDemo/CicadaDemo/CicadaCode/controller/MutlPlayViewController.h
@@ -0,0 +1,19 @@
+//
+//  MutlPlayViewController.h
+//  CicadaDemo
+//
+//  Created by ToT on 2020/4/23.
+//  Copyright © 2020 com.alibaba. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MutlPlayViewController : NSViewController
+
+@property (nonatomic,strong)NSArray <CicadaUrlSource *>*urlSourceArray;
+
+@end
+
+NS_ASSUME_NONNULL_END
