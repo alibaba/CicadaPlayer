02cf3741e4646eb9efa589c8bfe73105ec7c02c8
skufly <skufly@163.com>
2020-04-30 12:31:27 +0800

    feat(Demo): add sample code of CicadaAudioSessionDelegate

--- platform/Apple/demo/iOS/CicadaDemo/CicadaDemo/CicadaCode/controller/CicadaPlayerViewController.m
+++ platform/Apple/demo/iOS/CicadaDemo/CicadaDemo/CicadaCode/controller/CicadaPlayerViewController.m
@@ -18,7 +18,7 @@
 #import "CicadaErrorModel+string.h"
 #import "AFNetworking.h"
 
-@interface CicadaPlayerViewController ()<CicadaDemoViewDelegate,CicadaSettingAndConfigViewDelegate,CicadaDelegate>
+@interface CicadaPlayerViewController ()<CicadaDemoViewDelegate,CicadaSettingAndConfigViewDelegate,CicadaDelegate,CicadaAudioSessionDelegate>
 
 /**
  播放视图
@@ -70,6 +70,11 @@
 */
 @property (nonatomic,assign)BOOL needStop;
 
+/**
+混音播放
+*/
+@property (nonatomic,assign)BOOL enableMix;
+
 @end
 
 @implementation CicadaPlayerViewController
@@ -97,7 +102,8 @@
     [self.settingAndConfigView setIshardwareDecoder:[CicadaTool isHardware]];
     [self.settingAndConfigView setConfigArray:configArray];
     [self.view addSubview:self.settingAndConfigView];
-    
+
+    [CicadaPlayer setAudioSessionDelegate:self];
     self.player = [[CicadaPlayer alloc] init];
     self.player.enableHardwareDecoder = [CicadaTool isHardware];
     self.player.playerView = self.CicadaView.playerView;
@@ -163,6 +169,7 @@
     }
     [self.player stop];
     [self.player destroy];
+    [CicadaPlayer setAudioSessionDelegate:nil];
     [self setScreenCanRotation:NO];
 }
 
@@ -762,11 +769,31 @@ tableview点击外挂字幕回调
     [self.CicadaView setLoadingViewProgress:(int)progress];
 }
 
+- (BOOL)setActive:(BOOL)active error:(NSError **)outError
+{
+    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+    return [[AVAudioSession sharedInstance] setActive:active error:outError];
+}
+
+- (BOOL)setCategory:(NSString *)category withOptions:(AVAudioSessionCategoryOptions)options error:(NSError **)outError
+{
+//    self.enableMix = YES;
+    if (self.enableMix) {
+        options = AVAudioSessionCategoryOptionMixWithOthers | AVAudioSessionCategoryOptionDuckOthers;
+    }
+    return [[AVAudioSession sharedInstance] setCategory:category withOptions:options error:outError];
+}
+
+- (BOOL)setCategory:(AVAudioSessionCategory)category mode:(AVAudioSessionMode)mode routeSharingPolicy:(AVAudioSessionRouteSharingPolicy)policy options:(AVAudioSessionCategoryOptions)options error:(NSError **)outError
+{
+    if (self.enableMix) {
+        return YES;
+    }
+
+    if (@available(iOS 11.0, tvOS 11.0, *)) {
+        return [[AVAudioSession sharedInstance] setCategory:category mode:mode routeSharingPolicy:policy options:options error:outError];
+    }
+    return NO;
+}
+
 @end
-
-
-
-
-
-
-
