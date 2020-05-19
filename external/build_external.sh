9a7c5a641b8d7fec319d646464abe2c7b6d44994
pingkai <pingkai010@gmail.com>
2020-05-19 14:56:32 +0800

    feat(ffmpeg): enable dash demuxer
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- external/build_external.sh
+++ external/build_external.sh
@@ -25,6 +25,7 @@ function patch_ffmpeg() {
     git_am_patch ../../contribute/ffmpeg/0006-flv-add-extensions-for-H.265-HEVC.patch
     git_am_patch ../../contribute/ffmpeg/0007-build-libavformat-compile-avc.c-and-hevc.c-default.patch
     git_am_patch ../../contribute/ffmpeg/0008-avformat-flvdec-add-aliyun-extend-auio-codec-id.patch
+    git_am_patch ../../contribute/ffmpeg/0009-chore-configure-xml2-use-check-lib.patch
 }
 
 function git_apply_patch() {
