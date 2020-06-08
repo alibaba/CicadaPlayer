c7c7d09412eac9a384641ea90b9c6b40d60c3fb7
pingkai <pingkai010@gmail.com>
2020-06-08 15:32:14 +0800

    improvement(pbafframe): support 420v to avafframe
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- framework/base/media/PBAFFrame.cpp
+++ framework/base/media/PBAFFrame.cpp
@@ -42,6 +42,35 @@ PBAFFrame::PBAFFrame(CVPixelBufferRef pixelBuffer, int64_t pts, int64_t duration
         mInfo.video.colorSpace = COLOR_SPACE_UNSPECIFIED;
     }
 }
+static AVFrame *nv122yuv420p(AVFrame *frame)
+{
+
+    int x, y;
+    AVFrame *outFrame = av_frame_alloc();
+    outFrame->height = frame->height;
+    outFrame->width = frame->width;
+    outFrame->format = AV_PIX_FMT_YUV420P;
+    int ret = av_frame_get_buffer(outFrame, 32);
+    ret = av_frame_make_writable(outFrame);
+
+    if (frame->linesize[0] == frame->width) {
+        memcpy(outFrame->data[0], frame->data[0], frame->width * frame->height);
+    } else {
+        for (y = 0; y < outFrame->height; ++y) {
+            for (x = 0; x < outFrame->width; ++x) {
+                outFrame->data[0][y * outFrame->linesize[0] + x] = frame->data[0][y * frame->linesize[0] + x];
+            }
+        }
+    }
+
+    for (y = 0; y < outFrame->height / 2; ++y) {
+        for (x = 0; x < outFrame->width / 2; ++x) {
+            outFrame->data[1][y * outFrame->linesize[1] + x] = frame->data[1][y * frame->linesize[1] + 2 * x];
+            outFrame->data[2][y * outFrame->linesize[2] + x] = frame->data[1][y * frame->linesize[1] + 2 * x + 1];
+        }
+    }
+    return outFrame;
+}
 
 PBAFFrame::operator AVAFFrame *()
 {
@@ -51,11 +80,13 @@ PBAFFrame::operator AVAFFrame *()
     int planes, ret, i;
     OSType pixel_format = CVPixelBufferGetPixelFormatType(mPBuffer);
 
-    if (pixel_format != kCVPixelFormatType_420YpCbCr8Planar)
-        return nullptr;
-
     AVFrame *pFrame = av_frame_alloc();
-    pFrame->format = AV_PIX_FMT_YUV420P;
+    if (pixel_format == kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange || pixel_format == kCVPixelFormatType_420YpCbCr8BiPlanarFullRange) {
+        pFrame->format = AV_PIX_FMT_NV12;
+    } else if (pixel_format == kCVPixelFormatType_420YpCbCr8Planar) {
+        pFrame->format = AV_PIX_FMT_YUV420P;
+    } else
+        return nullptr;
     pFrame->width = (int) CVPixelBufferGetWidth(mPBuffer);
     pFrame->height = (int) CVPixelBufferGetHeight(mPBuffer);
     ret = av_frame_get_buffer(pFrame, 32);
@@ -88,7 +119,13 @@ PBAFFrame::operator AVAFFrame *()
                   (const uint8_t **) data, linesize, static_cast<AVPixelFormat>(pFrame->format),
                   pFrame->width, pFrame->height);
     CVPixelBufferUnlockBaseAddress(mPBuffer, kCVPixelBufferLock_ReadOnly);
+    if (pFrame->format == AV_PIX_FMT_NV12) {
+        AVFrame *pyvFrame = nv122yuv420p(pFrame);
+        av_frame_free(&pFrame);
+        pFrame = pyvFrame;
+    }
     pFrame->pts = mInfo.pts;
+    pFrame->pkt_duration = mInfo.duration;
     AVAFFrame *pAvFrame = new AVAFFrame(pFrame, FrameTypeVideo);
     av_frame_free(&pFrame);
     return pAvFrame;
