b36d557ea619003dbfdad8dd1b1831f15250e04c
pingkai <pingkai010@gmail.com>
2020-06-28 10:57:20 +0800

    feat(platform): add DCA event api
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- platform/Apple/source/CicadaErrorCode.h
+++ platform/Apple/source/CicadaErrorCode.h
@@ -55,47 +55,50 @@ typedef enum CicadaErrorCode: NSUInteger {
 
 /**@brief 播放器事件类型，带描述字符串*/
 /****@brief Player event type with a description string.*/
-typedef enum CicadaEventWithString: NSUInteger {
+typedef enum CicadaEventWithString : NSUInteger {
     /**@brief 设置了硬解，但是切换为软解。额外信息为描述信息。*/
     /****@brief Hardware decoding is switched to software decoding. Additional information: description.*/
     CICADA_EVENT_SWITCH_TO_SOFTWARE_DECODER = 100,
     /**@brief 音频解码格式不支持。额外信息为描述信息。*/
     /****@brief Audio decoding does not support the specified format. Additional information: description.*/
-    CICADA_EVENT_AUDIO_CODEC_NOT_SUPPORT    = 101,
+    CICADA_EVENT_AUDIO_CODEC_NOT_SUPPORT = 101,
     /**@brief 音频解码器设备失败。额外信息为描述信息。*/
     /****@brief The audio decoder failed. Additional information: description.*/
     CICADA_EVENT_AUDIO_DECODER_DEVICE_ERROR = 102,
     /**@brief 视频解码格式不支持。额外信息为描述信息。*/
     /****@brief Video decoding does not support the specified format. Additional information: description.*/
-    CICADA_EVENT_VIDEO_CODEC_NOT_SUPPORT    = 103,
+    CICADA_EVENT_VIDEO_CODEC_NOT_SUPPORT = 103,
     /**@brief 视频解码器设备失败。额外信息为描述信息。*/
     /****@brief The video decoder failed. Additional information: description.*/
     CICADA_EVENT_VIDEO_DECODER_DEVICE_ERROR = 104,
     /**@brief 视频渲染设备初始化失败。额外信息为描述信息。*/
     /****@brief The video renderer failed. Additional information: description.*/
-    CICADA_EVENT_VIDEO_RENDER_INIT_ERROR    = 105,
+    CICADA_EVENT_VIDEO_RENDER_INIT_ERROR = 105,
 
     /**@brief 网络失败，需要重试。无额外信息。*/
     /****@brief Network connection failed. Try again. No additional information is provided.*/
-    CICADA_EVENT_PLAYER_NETWORK_RETRY       = 108,
+    CICADA_EVENT_PLAYER_NETWORK_RETRY = 108,
     /**@brief 缓存成功。无额外信息。*/
     /****@brief Content cached. No additional information is provided.*/
-    CICADA_EVENT_PLAYER_CACHE_SUCCESS       = 109,
+    CICADA_EVENT_PLAYER_CACHE_SUCCESS = 109,
     /**@brief 缓存失败。额外信息为描述信息。*/
     /****@brief Failed to cache the content. Additional information: description.*/
-    CICADA_EVENT_PLAYER_CACHE_ERROR         = 110,
+    CICADA_EVENT_PLAYER_CACHE_ERROR = 110,
     /**@brief 系统无可用内存来存放媒体数据。*/
     /****@brief The system does not have memory to store the media data.*/
-    CICADA_EVENT_PLAYER_LOW_MEMORY          = 111,
+    CICADA_EVENT_PLAYER_LOW_MEMORY = 111,
     /**@brief 网络重试成功。无额外信息。*/
     /****@brief Network retry successful. No additional information is provided.*/
-    CICADA_EVENT_PLAYER_NETWORK_RETRY_SUCCESS             = 113,
+    CICADA_EVENT_PLAYER_NETWORK_RETRY_SUCCESS = 113,
     /**@brief 选择字幕错误。*/
     /****@brief select subtitle error.*/
     CICADA_EVENT_MEDIA_PLAYER_EVENT_SUBTITLE_SELECT_ERROR = 114,
     /**@brief 后台恢复时所需解码帧过多 */
     /****@brief  decoder recover size is too large for background recovery.*/
-    CICADA_EVENT_PLAYER_EVENT_DECODER_RECOVER_SIZE        = 115,
+    CICADA_EVENT_PLAYER_EVENT_DECODER_RECOVER_SIZE = 115,
+    /**@brief 播放组件中透传输出的消息 */
+    /****@brief The message passthrough the player component*/
+    CICADA_EVENT_PLAYER_DIRECT_COMPONENT_MSG = 116,
 } CicadaEventWithString;
 
 #endif
