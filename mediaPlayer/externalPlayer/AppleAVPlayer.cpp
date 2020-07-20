7c7783f4faea535a1fc45d563bc92692c23a1dd4
pingkai <pingkai010@gmail.com>
2020-07-20 17:54:33 +0800

    feat(mediaplayer): add dummy externalPlayer impl for jni and apple
    Signed-off-by: pingkai <pingkai010@gmail.com>

---
+++ mediaPlayer/externalPlayer/AppleAVPlayer.cpp
@@ -0,0 +1,6 @@
+//
+// Created by moqi on 2020/7/20.
+//
+
+#include "AppleAVPlayer.h"
+using namespace Cicada;
---
+++ mediaPlayer/externalPlayer/AppleAVPlayer.h
@@ -0,0 +1,15 @@
+//
+// Created by moqi on 2020/7/20.
+//
+
+#ifndef CICADAMEDIA_APPLE_AVPLAYER_H
+#define CICADAMEDIA_APPLE_AVPLAYER_H
+
+#include "../CicadaPlayerPrototype.h"
+namespace Cicada {
+    class AppleAVPlayer : public ICicadaPlayer, private CicadaPlayerPrototype {
+    };
+}// namespace Cicada
+
+
+#endif//CICADAMEDIA_APPLE_AVPLAYER_H
