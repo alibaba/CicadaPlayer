d7ce718e04a787e0761fb22c2b9f2cc09ec5936f
pingkai <pingkai010@gmail.com>
2020-07-20 17:54:33 +0800

    feat(mediaplayer): add dummy externalPlayer impl for jni and apple
    Signed-off-by: pingkai <pingkai010@gmail.com>

---
+++ mediaPlayer/externalPlayer/JNIPlayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by moqi on 2020/7/20.
+//
+
+#include "JNIPlayer.h"
---
+++ mediaPlayer/externalPlayer/JNIPlayer.h
@@ -0,0 +1,13 @@
+//
+// Created by moqi on 2020/7/20.
+//
+
+#ifndef CICADAMEDIA_JNIPLAYER_H
+#define CICADAMEDIA_JNIPLAYER_H
+
+#include "../CicadaPlayerPrototype.h"
+class JNIPlayer : public ICicadaPlayer, private CicadaPlayerPrototype {
+};
+
+
+#endif//CICADAMEDIA_JNIPLAYER_H
