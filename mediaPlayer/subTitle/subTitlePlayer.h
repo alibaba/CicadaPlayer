cf4b8521769df44707961533d9d716aaaaf07e39
pingkai <pingkai010@gmail.com>
2021-02-05 10:56:43 +0800

    refactor(subtitleplayer): improve async flush
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- mediaPlayer/subTitle/subTitlePlayer.h
+++ mediaPlayer/subTitle/subTitlePlayer.h
@@ -54,7 +54,7 @@ namespace Cicada {
 
             std::unique_ptr<subTitleSource> mSource;
             bool mSelected{false};
-            bool mNeedFlush{false};
+            std::atomic_int mNeedFlush{0};
             std::unique_ptr<IAFPacket> mPacket;
             int64_t mDelay{0};
             std::deque<std::unique_ptr<IAFPacket>> mSubtitleShowedQueue{};
