566e5f17382c43415ebb98ef088b316279fe4dad
kongjianneu <kongjianneu@126.com>
2021-03-04 19:14:57 +0800

    feature(dash): add libxml2 for windows

--- external/win/install_external.bat
+++ external/win/install_external.bat
@@ -2,5 +2,5 @@ git clone https://github.com/Microsoft/vcpkg.git
 cd vcpkg
 .\bootstrap-vcpkg.sh
 .\vcpkg integrate install
-.\vcpkg install openssl curl[openssl] sdl2 pthread dirent
+.\vcpkg install openssl curl[openssl] sdl2 pthread dirent libxml2
 pause
