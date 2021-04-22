d329891b13e2048324f323400bb47d6e6663ec1d
lifujun <814084764@qq.com>
2021-04-22 12:39:44 +0800

    improve(RenderCB): add autoreleasepool
    Signed-off-by: lifujun <814084764@qq.com>

--- platform/Apple/source/CicadaRenderCBWrapper.mm
+++ platform/Apple/source/CicadaRenderCBWrapper.mm
@@ -4,34 +4,36 @@
 
 bool CicadaRenderCBWrapper::OnRenderFrame(void *userData, IAFFrame *frame)
 {
-    id<CicadaRenderDelegate> delegate = (__bridge id<CicadaRenderDelegate>)userData;
-    if (nullptr == delegate) {
+    @autoreleasepool {
+        id<CicadaRenderDelegate> delegate = (__bridge id<CicadaRenderDelegate>) userData;
+        if (nullptr == delegate) {
+            return false;
+        }
+
+        if (frame->getType() == IAFFrame::FrameTypeVideo) {
+            switch (frame->getInfo().video.format) {
+                case AF_PIX_FMT_APPLE_PIXEL_BUFFER: {
+                    auto *ppBFrame = dynamic_cast<PBAFFrame *>(frame);
+                    CVPixelBufferRef pixelBuffer = ppBFrame->getPixelBuffer();
+                    if ([delegate respondsToSelector:@selector(onVideoPixelBuffer:pts:)]) {
+                        return [delegate onVideoPixelBuffer:pixelBuffer pts:frame->getInfo().pts];
+                    }
+                    break;
+                }
+                case AF_PIX_FMT_YUV420P:
+                    if ([delegate respondsToSelector:@selector(onVideoPixelBuffer:pts:)]) {
+                        return [delegate onVideoRawBuffer:frame->getData()
+                                                 lineSize:frame->getLineSize()
+                                                      pts:frame->getInfo().pts
+                                                    width:frame->getInfo().video.width
+                                                   height:frame->getInfo().video.height];
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
         return false;
     }
-
-    if (frame->getType() == IAFFrame::FrameTypeVideo) {
-        switch (frame->getInfo().video.format) {
-            case AF_PIX_FMT_APPLE_PIXEL_BUFFER: {
-                auto *ppBFrame = dynamic_cast<PBAFFrame *>(frame);
-                CVPixelBufferRef pixelBuffer = ppBFrame->getPixelBuffer();
-                if ([delegate respondsToSelector:@selector(onVideoPixelBuffer:pts:)]){
-                    return [delegate onVideoPixelBuffer:pixelBuffer pts:frame->getInfo().pts];
-                }
-                break;
-            }
-            case AF_PIX_FMT_YUV420P:
-                if ([delegate respondsToSelector:@selector(onVideoPixelBuffer:pts:)]){
-                    return [delegate onVideoRawBuffer:frame->getData()
-                                             lineSize:frame->getLineSize()
-                                                  pts:frame->getInfo().pts
-                                                width:frame->getInfo().video.width
-                                               height:frame->getInfo().video.height];
-                }
-                break;
-            default:
-                break;
-        }
-    }
-
-    return false;
 }
