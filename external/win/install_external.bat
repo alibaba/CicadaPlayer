a72db805aff9ad33f5be481896f092ed4a4540f9
kongjianneu <kongjianneu@126.com>
2021-05-12 15:37:47 +0800

    build(windows): specify vcpkg version

--- external/win/install_external.bat
+++ external/win/install_external.bat
@@ -1,5 +1,6 @@
 git clone https://github.com/Microsoft/vcpkg.git
 cd vcpkg
+git checkout 5a271a9290282e09149401486f88dc106dc65b71
 .\bootstrap-vcpkg.sh
 .\vcpkg integrate install
 .\vcpkg install openssl curl[openssl] sdl2 pthread dirent libxml2
