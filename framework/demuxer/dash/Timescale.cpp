7fa4f51ba54ee2ada1a1f19a0785e75d6ffdc91e
kongjianneu <kongjianneu@126.com>
2021-05-28 14:26:30 +0800

    fix(Timescale): check parameters to avoid integer overflow

--- framework/demuxer/dash/Timescale.cpp
+++ framework/demuxer/dash/Timescale.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "Timescale.h"
+
+using namespace Cicada::Dash;
+
+Timescale::Timescale(uint64_t v) : scale(v)
+{}
+
+int64_t Timescale::ToTime(int64_t t) const
+{
+    if (!scale) return 0;
+    int64_t v = t / scale;
+    if (v > INT64_MAX / 1000000) {
+        return 0;
+    }
+    int64_t r = t % scale;
+    return v * 1000000 + r * 1000000 / scale;
+}
+
+int64_t Timescale::ToScaled(int64_t t) const
+{
+    int64_t v = t / 1000000;
+    if (scale == 0 || v > INT64_MAX / scale) {
+        return 0;
+    }
+    int64_t r = t % 1000000;
+    return v * scale + r * scale / 1000000;
+}
+
+bool Timescale::isValid() const
+{
+    return !!scale;
+}
+
+Timescale::operator uint64_t() const
+{
+    return scale;
+}
--- framework/demuxer/dash/Timescale.h
+++ framework/demuxer/dash/Timescale.h
@@ -11,32 +11,12 @@ namespace Cicada {
     namespace Dash {
         class Timescale {
         public:
-            Timescale(uint64_t v = 0) : scale(v)
-            {}
+            Timescale(uint64_t v = 0);
 
-            int64_t ToTime(int64_t t) const
-            {
-                if (!scale) return 0;
-                int64_t v = t / scale;
-                int64_t r = t % scale;
-                return v * 1000000 + r * 1000000 / scale;
-            }
-
-            int64_t ToScaled(int64_t t) const
-            {
-                int64_t v = t / 1000000;
-                int64_t r = t % 1000000;
-                return v * scale + r * scale / 1000000;
-            }
-
-            bool isValid() const
-            {
-                return !!scale;
-            }
-            operator uint64_t() const
-            {
-                return scale;
-            }
+            int64_t ToTime(int64_t t) const;
+            int64_t ToScaled(int64_t t) const;
+            bool isValid() const;
+            operator uint64_t() const;
 
         private:
             uint64_t scale;
