065a9d06b01a8b174b7b1e874bbd535e6313e5c7
kongjianneu <kongjianneu@126.com>
2021-06-08 15:31:32 +0800

    build(windows): revert the version of sdl2 to 2.0.12
    (cherry picked from commit 1b15357a6baa1e3039f68e44aab9ae2e68841ecb)
    Signed-off-by: kongjianneu <kongjianneu@126.com>

--- external/win/install_external.bat
+++ external/win/install_external.bat
@@ -1,7 +1,10 @@
 git clone https://github.com/Microsoft/vcpkg.git
 cd vcpkg
-git checkout 5a271a9290282e09149401486f88dc106dc65b71
+git checkout 109ce457421dbef04011a7e5a4fd482f61e21f62
 .\bootstrap-vcpkg.sh
 .\vcpkg integrate install
-.\vcpkg install openssl curl[openssl] sdl2 pthread dirent libxml2
+.\vcpkg install openssl sdl2 pthread dirent libxml2
+git checkout 5a271a9290282e09149401486f88dc106dc65b71
+.\bootstrap-vcpkg.bat
+.\vcpkg install curl[openssl]
 pause
