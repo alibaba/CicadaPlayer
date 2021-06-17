328512eadd86c25488e035062aa442aa4ac5b7c1
pingkai <pingkai010@gmail.com>
2021-06-17 15:12:02 +0800

    refactor(smp): not handle audio render interrupted
    Signed-off-by: pingkai <pingkai010@gmail.com>
    (cherry picked from commit 0080e7e9856b292ca7396310122f0d5f3f2b502c)

--- mediaPlayer/SuperMediaPlayer.h
+++ mediaPlayer/SuperMediaPlayer.h
@@ -368,22 +368,6 @@ namespace Cicada {
 
             void onEOS() override
             {}
-
-            bool onInterrupt(bool interrupt) override
-            {
-                if (interrupt) {
-                    if (mPlayer.mPlayStatus == PLAYER_PLAYING) {
-                        mPlayer.Pause();
-                        mPlayer.mPausedByAudioInterrupted = true;
-                    }
-                } else {
-                    if (mPlayer.mPlayStatus == PLAYER_PAUSED && mPlayer.mPausedByAudioInterrupted) {
-                        mPlayer.Start();
-                    }
-                }
-
-                return true;
-            }
             void onFrameInfoUpdate(IAFFrame::AFFrameInfo &info, bool rendered) override;
 
         private:
@@ -547,7 +531,6 @@ namespace Cicada {
         void *mVideoRenderingCbUserData{nullptr};
 
         bool mIsDummy{false};
-        bool mPausedByAudioInterrupted{false};
         readCB mBSReadCb = nullptr;
         seekCB mBSSeekCb = nullptr;
         void *mBSCbArg = nullptr;
