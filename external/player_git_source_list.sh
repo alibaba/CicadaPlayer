db412addb4fe1133ee57d19e369fbb5cdd3ba04b
pingkai <pingkai010@gmail.com>
2021-06-18 13:32:09 +0800

    build(ffmpeg): update to 4.3.1
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- external/player_git_source_list.sh
+++ external/player_git_source_list.sh
@@ -12,7 +12,7 @@ if [[ -z "${FFMPEG_GIT}" ]];then
     FFMPEG_GIT=https://github.com/FFmpeg/FFmpeg.git
 fi
 if [[ -z "${FFMPEG_BRANCH}" ]];then
-    FFMPEG_BRANCH=n4.2.1
+    FFMPEG_BRANCH=n4.3.1
 fi
 
 if [[ -z "${FFMPEG_NEED_PATCH}" ]];then
