f0c0e4fecc63190cc02adad16988b50484e93ae1
pingkai <pingkai010@gmail.com>
2021-07-26 18:03:09 +0800

    refactor(cheateraudiorender): improve CheaterAudioRender
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- framework/render/audio/CheaterAudioRender.h
+++ framework/render/audio/CheaterAudioRender.h
@@ -12,14 +12,10 @@ namespace Cicada {
     class CheaterAudioRender : public filterAudioRender {
     public:
         CheaterAudioRender()
-        {
-
-        }
+        {}
 
         ~CheaterAudioRender() override
-        {
-
-        }
+        {}
 
     private:
         int init_device() override;
@@ -31,14 +27,10 @@ namespace Cicada {
         void flush_device() override;
 
         void device_setVolume(float gain) override
-        {
-
-        }
+        {}
 
         void device_mute(bool bMute) override
-        {
-
-        }
+        {}
 
         int64_t device_get_position() override;
 
@@ -50,14 +42,40 @@ namespace Cicada {
         {
             return 0;
         }
+        bool device_require_format(const IAFFrame::audioInfo &info) override
+        {
+            return true;
+        }
+
+    private:
+        class positionInfo {
+        public:
+            positionInfo(unique_ptr<IAFFrame> &frame, int64_t position) : mFrame(std::move(frame)), mPosition(position)
+            {}
+            ~positionInfo() = default;
+
+            int64_t getPosition() const
+            {
+                return mPosition;
+            }
+
+            IAFFrame *getFrame()
+            {
+                return mFrame.get();
+            }
+
+        private:
+            unique_ptr<IAFFrame> mFrame;
+            int64_t mPosition;
+        };
 
     private:
         af_clock mClock{};
         int64_t mPCMDuration{0};
-
-
+        std::mutex mMutex;
+        std::queue<std::unique_ptr<positionInfo>> mQueue;
     };
-}
+}// namespace Cicada
 
 
-#endif //CICADAMEDIA_CHEATERAUDIORENDER_H
+#endif//CICADAMEDIA_CHEATERAUDIORENDER_H
