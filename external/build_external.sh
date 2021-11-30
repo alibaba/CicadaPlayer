b997a2b72eda42eaf9f1167fc537439800093f57
pingkai <pingkai010@gmail.com>
2021-11-30 13:28:19 +0800

    refactor(contribute): add flv patch to speed up flv playback when video/audio only stream
    Signed-off-by: pingkai <pingkai010@gmail.com>

--- external/build_external.sh
+++ external/build_external.sh
@@ -27,6 +27,7 @@ function patch_ffmpeg() {
     git_am_patch ../../contribute/ffmpeg/0008-avformat-flvdec-add-aliyun-extend-auio-codec-id.patch
     git_am_patch ../../contribute/ffmpeg/0009-chore-configure-xml2-use-check-lib.patch
     git_am_patch ../../contribute/ffmpeg/0010-build-export-some-func-for-windows.patch
+    git_am_patch ../../contribute/ffmpeg/0011-libavformat-flvdec-add-flv_strict_header-option-to-d.patch
 }
 
 function patch_openssl() {
